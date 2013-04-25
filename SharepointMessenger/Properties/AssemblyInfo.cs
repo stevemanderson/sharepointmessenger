ffa692423fa74abca9955051681583bd282e74f8
Steve Anderson <steveanderson@hotmail.ca>
2013-04-25 19:59:06 -0400

    update to the naming messager to messenger

--- SharepointMessenger/Properties/AssemblyInfo.cs
+++ SharepointMessenger/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharepointMessenger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sharepoint Messenger")]
+[assembly: AssemblyDescription("Private Messager for Sharepoint 2010")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharepointMessenger")]
+[assembly: AssemblyCompany("Steve Anderson")]
+[assembly: AssemblyProduct("Sharepoint Messenger")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
